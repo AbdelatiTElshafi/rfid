9bc8a2345ba0c6a0d80cfe6fb91c76f911bd0bae
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-22 23:10:56 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/saved_tags/saved_tags_widget.dart
+++ lib/pages/saved_tags/saved_tags_widget.dart
@@ -147,53 +147,55 @@ class _SavedTagsWidgetState extends State<SavedTagsWidget> {
           top: true,
           child: Padding(
             padding: EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 0.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Container(
-                  decoration: BoxDecoration(),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Builder(
-                          builder: (context) {
-                            final itemAtIndex = _model.tagsID.toList();
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Builder(
+                            builder: (context) {
+                              final itemAtIndex = _model.tagsID.toList();
 
-                            return ListView.separated(
-                              padding: EdgeInsets.zero,
-                              shrinkWrap: true,
-                              scrollDirection: Axis.vertical,
-                              itemCount: itemAtIndex.length,
-                              separatorBuilder: (_, __) =>
-                                  SizedBox(height: 10.0),
-                              itemBuilder: (context, itemAtIndexIndex) {
-                                final itemAtIndexItem =
-                                    itemAtIndex[itemAtIndexIndex];
-                                return TagIDWidget(
-                                  key: Key(
-                                      'Keyut8_${itemAtIndexIndex}_of_${itemAtIndex.length}'),
-                                  name: _model.name
-                                      .elementAtOrNull(itemAtIndexIndex),
-                                  serial: _model.serials
-                                      .elementAtOrNull(itemAtIndexIndex),
-                                  tagId: _model.tagsID
-                                      .elementAtOrNull(itemAtIndexIndex),
-                                  partNo: _model.partNOs
-                                      .elementAtOrNull(itemAtIndexIndex),
-                                  rebuild: () async {
-                                    safeSetState(() {});
-                                  },
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ],
+                              return ListView.separated(
+                                padding: EdgeInsets.zero,
+                                shrinkWrap: true,
+                                scrollDirection: Axis.vertical,
+                                itemCount: itemAtIndex.length,
+                                separatorBuilder: (_, __) =>
+                                    SizedBox(height: 10.0),
+                                itemBuilder: (context, itemAtIndexIndex) {
+                                  final itemAtIndexItem =
+                                      itemAtIndex[itemAtIndexIndex];
+                                  return TagIDWidget(
+                                    key: Key(
+                                        'Keyut8_${itemAtIndexIndex}_of_${itemAtIndex.length}'),
+                                    name: _model.name
+                                        .elementAtOrNull(itemAtIndexIndex),
+                                    serial: _model.serials
+                                        .elementAtOrNull(itemAtIndexIndex),
+                                    tagId: _model.tagsID
+                                        .elementAtOrNull(itemAtIndexIndex),
+                                    partNo: _model.partNOs
+                                        .elementAtOrNull(itemAtIndexIndex),
+                                    rebuild: () async {
+                                      safeSetState(() {});
+                                    },
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
