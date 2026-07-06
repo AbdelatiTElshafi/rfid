6489830c72c582c3e4123af11244a990f19b2895
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-07-06 10:04:38 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/start_page/start_page_widget.dart
+++ lib/pages/start_page/start_page_widget.dart
@@ -40,6 +40,25 @@ class _StartPageWidgetState extends State<StartPageWidget> {
       } else {
         FFAppState().rfidConnected = false;
         safeSetState(() {});
+        var confirmDialogResponse = await showDialog<bool>(
+              context: context,
+              builder: (alertDialogContext) {
+                return AlertDialog(
+                  title: Text('TEST'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(alertDialogContext, false),
+                      child: Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.pop(alertDialogContext, true),
+                      child: Text('Confirm'),
+                    ),
+                  ],
+                );
+              },
+            ) ??
+            false;
       }
 
       context.pushNamed(HomeWidget.routeName);
